39a1c8e8bbd63f9f7ff1b64a23d87a341affd34f
GitHub Action <action@github.com>
2021-10-24 02:26:46 +0000

    Template cleanup

--- src/main/kotlin/com/github/igaozp/sumba/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/igaozp/sumba/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.igaozp.sumba.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.igaozp.sumba.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/igaozp/sumba/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/igaozp/sumba/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.igaozp.sumba.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.igaozp.sumba.MyBundle
 
 class MyApplicationService {
 
